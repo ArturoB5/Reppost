f2d16fb37ad9911c7767809d9c7a6b66b67b0007
Arturo Badillo <108558846+ArturoB5@users.noreply.github.com>
2024-12-17 08:10:43 -0500

    Delete Código/Reppost directory

--- Código/Reppost/Config/dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=reppostdb;charset=utf8mb4', 'root', '');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec("SET NAMES 'utf8mb4'");
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
-}
