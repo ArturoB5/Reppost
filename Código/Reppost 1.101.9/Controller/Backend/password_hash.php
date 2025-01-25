653e6760b1f85c7becb8696f7a0e5dc63f4d87b6
Arturo Badillo <108558846+ArturoB5@users.noreply.github.com>
2025-01-25 12:18:47 -0500

    Delete Código/Reppost 1.101.9 directory

--- Código/Reppost 1.101.9/Controller/Backend/password_hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = $_POST['password'];
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-$conn->query("INSERT INTO members (username, email, password, firstname, lastname, gender, image) VALUES ('$username', '$email', '$hashed_password', '$firstname', '$lastname', '$gender', 'View/Images/all_images/No_Photo_Available.jpg')");
