0478ce0fa6a5047e24a3def5b93d0a324d966691
Arturo Badillo <108558846+ArturoB5@users.noreply.github.com>
2025-01-25 12:20:34 -0500

    Add files via upload

---
+++ Código/Reppost 1.110.10/index.php
@@ -0,0 +1,203 @@
+<?php include('Config/dbcon.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Reppost</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="View/Images/app_images/logo.ico">
+    <link rel="stylesheet" type="text/css" href="View/css/demo.css" />
+    <link rel="stylesheet" type="text/css" href="View/css/style-index.css" />
+    <link rel="stylesheet" type="text/css" href="View/css/animate-custom.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="clr">
+            <div class="title"></div>
+            <a href="index.php">
+                <img class="logo" src="View/Images/app_images/logo.png" alt="Logo" width="150">
+            </a>
+        </div>
+        <section>
+            <div id="container_demo">
+                <a class="hiddenanchor" id="toregister"></a>
+                <a class="hiddenanchor" id="tologin"></a>
+                <a class="hiddenanchor" id="toforgot"></a>
+                <div id="wrapper">
+                    <div id="login" class="animate form">
+                        <form method="post" action="login.php">
+                            <div class="title">Reppost</div>
+                            <hr>
+                            <p style="position: relative;">
+                                <label for="username"> Usuario o Correo electrónico</label>
+                                <input id="username" name="username" required="required" type="text" />
+                                <i class="fa fa-user" style="position: absolute; left: 10px; top: 34px; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p style="position: relative;">
+                                <label for="password"> Contraseña </label>
+                                <input id="password" name="password" required="required" type="password" />
+                                <i class="fa fa-key" style="position: absolute; left: 10px; top: 34px; color: rgb(97 160 159)"></i>
+                                <span id="toggle-password" class="fa fa-eye" style="position: absolute; right: 10px; top: 35px; cursor: pointer; color: rgb(97 160 159)"></span>
+                            </p>
+                            <p class="login button">
+                                <input type="submit" name="login" value="Ingresar" />
+                            </p>
+                            <p>
+                                <center>
+                                    <a href="#toforgot">¿Olvidaste tu contraseña?</a>
+                                </center>
+                            </p>
+                            <p class="change_link">
+                                Registrate
+                                <a href="#toregister" class="to_register">Crear cuenta</a>
+                            </p>
+                        </form>
+                        <?php
+                        if (isset($_GET['error'])) {
+                            $error_message = '';
+                            switch ($_GET['error']) {
+                                case 'invalid_password':
+                                    $error_message = 'La contraseña es incorrecta.';
+                                    break;
+                                case 'user_not_found':
+                                    $error_message = 'El usuario no existe.';
+                                    break;
+                                case 'email_not_verified':
+                                    $error_message = 'Aún no te has verificado. Por favor revisa tu correo.';
+                                    break;
+                                default:
+                                    $error_message = 'Hubo un error desconocido.';
+                                    break;
+                            }
+                            echo "<div class='error-message'>$error_message</div>";
+                        }
+                        ?>
+                    </div>
+                    <!-- Sección REGISTER -->
+                    <div id="register" class="animate form">
+                        <form action="signup_save.php" method="post" autocomplete="on">
+                            <h2>REGISTRATE</h2>
+                            <hr>
+                            <p style="position: relative;">
+                                <label for="usernamesignup" class="uname">Nombre de usuario</label>
+                                <input id="usernamesignup" name="username" required="required" type="text" placeholder="Usuario" />
+                                <i class="fa fa-user" style="position: absolute; left: 10px; top: 35px; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p style="position: relative;">
+                                <label for="emailsignup" class="youmail">Correo electronico</label>
+                                <input id="emailsignup" name="email" required="required" type="email" placeholder="Correo electrónico" />
+                                <i class="fa fa-envelope" style="position: absolute; left: 10px; top: 35px; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p style="position: relative;">
+                                <label for="firstname">Nombre/s</label>
+                                <input id="firstname" name="firstname" required="required" type="text" placeholder="Nombre" />
+                                <i class="fa fa-pencil" style="position: absolute; left: 10px; top: 35px; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p style="position: relative;">
+                                <label for="lastname">Apellido/s</label>
+                                <input id="lastname" name="lastname" required="required" type="text" placeholder="Apellido" />
+                                <i class="fa fa-pencil" style="position: absolute; left: 10px; top: 35px; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p style="position: relative;">
+                                <label for="birthdate">Fecha de Nacimiento</label>
+                                <input id="birthdate" name="birthdate" required="required" type="date" />
+                                <i class="fa fa-calendar" style="position: absolute; left: 10px; top: 35px; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p style="position: relative;">
+                                <label for="gender">Género</label>
+                            <div class="gender-options">
+                                <input type="checkbox" id="hombre" name="gender" value="Hombre" onclick="onlyOne(this)" class="gender-checkbox">
+                                <label for="hombre">Hombre</label>
+                                <input type="checkbox" id="mujer" name="gender" value="Mujer" onclick="onlyOne(this)" class="gender-checkbox">
+                                <label for="mujer">Mujer</label>
+                                <input type="checkbox" id="otro" name="gender" value="Otro" onclick="onlyOne(this)" class="gender-checkbox">
+                                <label for="otro">Otro</label>
+                            </div>
+                            </p>
+                            <script>
+                                function onlyOne(checkbox) {
+                                    var checkboxes = document.getElementsByName('gender');
+                                    checkboxes.forEach((item) => {
+                                        if (item !== checkbox) item.checked = false;
+                                    });
+                                }
+                            </script>
+                            <p style="position: relative;">
+                                <label for="passwordsignup" style="position: relative; display: inline-block;">
+                                    Contraseña
+                                    <i class="fa fa-info-circle"
+                                        style="margin-left: 5px; color: #61A09F; cursor: pointer;"
+                                        title="La contraseña debe tener al menos:
+- 8 caracteres.
+- Una letra mayúscula.
+- Una letra minúscula.
+- Un número.
+- Un carácter especial (@, #, $, etc.).">
+                                    </i>
+                                </label>
+                                <input id="passwordsignup" name="password" required="required" type="password" placeholder="Contraseña" />
+                                <i class="fa fa-key" style="position: absolute; left: 10px; top: 33px; color: rgb(97 160 159)"></i>
+                                <i id="toggle-password-signup" class="fa fa-eye" style="position: absolute; right: 10px; top: 33px; cursor: pointer; color: rgb(97 160 159)"></i>
+                                <!-- Semáforo -->
+                            <div id="password-strength" style="margin-top: 10px; height: 10px; width: 100%; background-color: #ccc; border-radius: 5px;">
+                                <div id="strength-bar" style="height: 100%; width: 0%; border-radius: 5px;"></div>
+                            </div>
+                            <!-- Etiqueta de dificultad -->
+                            <span id="strength-text" style="font-size: 14px; color: #555; margin-top: 5px; display: inline-block;">
+                                Dificultad:
+                            </span>
+                            </p>
+                            <p style="position: relative;">
+                                <label for="confirm_password">Confirmar contraseña
+                                    <i class="fa fa-info-circle"
+                                        style="margin-left: 5px; color: #61A09F; cursor: pointer;"
+                                        title="La contraseña debe ser igual que la anterior">
+                                    </i>
+                                </label>
+                                <input id="confirm_password" type="password" name="confirm_password" placeholder="Confirmar contraseña" required="required">
+                                <i class="fa fa-key" style="position: absolute; left: 10px; top: 34px; color: rgb(97 160 159)"></i>
+                                <i id="toggle-password-confirm" class="fa fa-eye" style="position: absolute; right: 10px; top: 35px; cursor: pointer; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p class="signin button">
+                                <input type="submit" style="margin-top: 20px" value="Registrate" />
+                            </p>
+                            <p style="padding: 0px 0px 0px 30px" class="change_link">
+                                ¿Ya estás registrado?
+                                <a href="#tologin" style="margin-top: 20px" class="to_register">Ingresar</a>
+                            </p>
+                        </form>
+                    </div>
+                    <!-- Sección FORGOT PASSWORD -->
+                    <div id="forgot" class="animate form">
+                        <form action="forgot_password.php" method="post">
+                            <h4>RECUPERA TU CONTRASEÑA</h4>
+                            <hr>
+                            <p style="position: relative;">
+                                <label for="identifier">Ingresa tu correo o usuario</label>
+                                <input id="identifier" name="identifier" required="required" type="text" placeholder="Correo o usuario" />
+                                <i class="fa fa-envelope" style="position: absolute; left: 10px; top: 35px; color: rgb(97 160 159)"></i>
+                            </p>
+                            <p class="login button">
+                                <input type="submit" value="Enviar enlace" />
+                            </p>
+                            <p class="change_link">
+                                ¿Ya la recordaste?
+                                <a href="#tologin" class="to_register">Iniciar sesión</a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+<footer>
+    <script src="View/JS/pass_diff.js"></script>
+    <script src="View/JS/view_pass_form.js"></script>
+</footer>
+
+</html>
---
+++ Código/Reppost 1.110.10/save_edit.php
@@ -0,0 +1,32 @@
+<?php
+include('Config/dbcon.php');
+$member_id = $_POST['member_id'];
+// Obtener los datos actuales del usuario en la BD
+$stmtOld = $conn->prepare("SELECT * FROM members WHERE member_id = :member_id");
+$stmtOld->bindParam(':member_id', $member_id, PDO::PARAM_INT);
+$stmtOld->execute();
+$oldData = $stmtOld->fetch(PDO::FETCH_ASSOC);
+// Recupera los datos y evita sobrescribir valores vacíos
+$mobile  = !empty($_POST['mobile'])  ? $_POST['mobile']  : $oldData['mobile'];
+$gender  = !empty($_POST['gender'])  ? $_POST['gender']  : $oldData['gender'];
+$country = !empty($_POST['country']) ? $_POST['country'] : $oldData['country'];
+$city    = !empty($_POST['city'])    ? $_POST['city']    : $oldData['city'];
+$work    = !empty($_POST['work'])    ? $_POST['work']    : $oldData['work'];
+// Actualizar
+$stmtUpdate = $conn->prepare("
+    UPDATE members SET 
+        mobile  = :mobile,
+        gender  = :gender,
+        country = :country,
+        city    = :city,
+        work    = :work
+    WHERE member_id = :member_id
+");
+$stmtUpdate->bindParam(':mobile',     $mobile);
+$stmtUpdate->bindParam(':gender',     $gender);
+$stmtUpdate->bindParam(':country',    $country);
+$stmtUpdate->bindParam(':city',       $city);
+$stmtUpdate->bindParam(':work',       $work);
+$stmtUpdate->bindParam(':member_id',  $member_id, PDO::PARAM_INT);
+$stmtUpdate->execute();
+echo "<script>window.location = 'profile.php?id={$member_id}';</script>";
